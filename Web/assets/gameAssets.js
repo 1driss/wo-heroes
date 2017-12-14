4083b9a2dd1314f3ae80f6871dce3254d83708bb
1driss <34073412+1driss@users.noreply.github.com>
2017-12-14 14:17:11 -0600

    Add files via upload

---
+++ Web/assets/gameAssets.js
@@ -0,0 +1,12 @@
+function openForum(){window.open("http://www.facebook.com/woheroes");} /*Al presionar forums, se abre en otra ventana la página de facebook del juego.*/
+
+
+function gifteeSelector(){alert("Feature not available in beta.");}
+function invite(){alert("Feature not available in beta.");}
+function giftSelector(){alert("Feature not available in beta.");}
+function help(){alert("Feature not available in beta.");}
+function bank(){window.open("http://www.facebook.com/woheroes");}
+function profile(){alert("Feature not available in beta.");}
+function showInviteAll(){alert("Feature not available in beta.");}
+function sendSocialAction(){window.open("https://www.facebook.com/sharer/sharer.php?u=www.wildheroes.pw?noCache");}
+function switchView(view){var usr=$("#usr").val();switch(view){case"bank":break;}}
